19a4f7eeb73202e779e9c8aa78913d99c6248178
Julia Ch <juliecha@yandex.ru>
2023-02-09 23:19:15 +0300

    UPD array

--- Lec_1/program.java
+++ Lec_1/program.java
@@ -118,12 +118,43 @@ class Program {
 
 
         // Массивы
+
+        // Одномерные
         int[] arr = new int[10];
         System.out.println(arr.length); // 10
         arr = new int[] { 1, 2, 3, 4, 5 };
         System.out.println(arr.length); // 5
 
+        // Многомерные
+        int[] matr[] = new int[3][5];
+        for (int[] line : matr) {
+            for (int item : line) {
+                System.out.printf("%d ", item);
+            }
+        System.out.println();
+        }
+        
+        // Преобразования
+
+        int i = 123; double ad = i;
+        System.out.println(i); // 123
+        System.out.println(ad); // 123.0
+        d = 3.1415; i = (int)d;
+        System.out.println(d); // 3.1415
+        System.out.println(i); // 3
+        d = 3.9415; i = (int)d;
+        System.out.println(d); // 3.9415
+        System.out.println(i); // 3
+        byte b = Byte.parseByte("123");
+        System.out.println(b); // 123
+        b = Byte.parseByte("1234");
+        System.out.println(b); // NumberFormatException: Value out of range
+
+
+
     }
+
+
 
     static String getType(Object o){
         return o.getClass().getSimpleName();
