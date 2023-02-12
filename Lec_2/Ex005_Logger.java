e9fbdaac9bc882415db40bf1115ec15bcde92236
Julia Ch <juliecha@yandex.ru>
2023-02-12 21:44:47 +0300

    UPD Logger

--- Lec_2/Ex005_Logger.java
+++ Lec_2/Ex005_Logger.java
@@ -6,16 +6,16 @@ import java.util.logging.*;
 public class Ex005_Logger {
     public static void main(String[] args) throws IOException {
     
-        Logger logger = Logger.getLogger(Ex005_Logger.class.getName());
+        Logger logger = Logger.getLogger(Ex005_Logger.class.getName()); // имя класса Ex005_Logger.class.getName()
         //ConsoleHandler ch = new ConsoleHandler();
-        FileHandler fh = new FileHandler("Lec_2/log.txt");
+        FileHandler fh = new FileHandler("Lec_2/log.xml");
         //logger.addHandler(ch);
         logger.addHandler(fh);
         
-        SimpleFormatter sFormat = new SimpleFormatter();
-        //XMLFormatter xml = new XMLFormatter();
-        fh.setFormatter(sFormat);
-        //fh.setFormatter(xml);
+        // SimpleFormatter sFormat = new SimpleFormatter();
+        XMLFormatter xml = new XMLFormatter();
+        // fh.setFormatter(sFormat);
+        fh.setFormatter(xml);
         
         //logger.setLevel(Level.INFO);
         logger.log(Level.WARNING, "Тестовое логирование 1");
@@ -25,6 +25,16 @@ public class Ex005_Logger {
 }
 
 
+// Логирование. Использование. Основы
+// Использование
+// Logger logger = Logger.getLogger()
+// Уровни важности
+// INFO, DEBUG, ERROR, WARNING и др.
+// Вывод
+// ConsoleHandler info = new ConsoleHandler();
+// log.addHandler(info);
+// Формат вывода: структурированный, абы как*
+// XMLFormatter, SimpleFormatter
 
 
 
