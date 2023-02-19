ab70c73f0120e338299dbde28d9d915117587d0b
Julia Ch <juliecha@yandex.ru>
2023-02-19 11:47:57 +0300

    UPD Ex006 - List.of()

--- Lec_3/Ex006.java
+++ Lec_3/Ex006.java
@@ -5,9 +5,10 @@ import java.util.List;
 public class Ex006 {
     public static void main(String[] args) {
         Character value = null;
+        // List.of(item1, item2,...) – возвращает неизменяемый список
         List<Character> list1 = List.of('S', 'e', 'r', 'g', 'e', 'y');
         System.out.println(list1);
-        list1.remove(1); // java.lang.UnsupportedOperationException
+        // list1.remove(1); // java.lang.UnsupportedOperationException  При создании колекции через List.of просто так удалять элементы через .remove(1) не получится, программа не скомпилируется
         List<Character> list2 = List.copyOf(list1);
         
     }
