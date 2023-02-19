70bb60477f69651e3d1c42d20b139e2848f4ed88
Julia Ch <juliecha@yandex.ru>
2023-02-19 11:43:46 +0300

    UPD Ex005 - StringBuildeer methods

--- Lec_3/Ex005.java
+++ Lec_3/Ex005.java
@@ -8,9 +8,22 @@ public class Ex005 {
         StringBuilder day = new StringBuilder("28");
         StringBuilder month = new StringBuilder("9");
         StringBuilder year = new StringBuilder("1990");
-        List<StringBuilder> d = Arrays.asList(day, month, year);
+        StringBuilder[] date = { day, month, year };
+        List<StringBuilder> d = Arrays.asList(date);
         System.out.println(d); // [29, 9, 1990]
-        day = new StringBuilder("09");
+        date[1] = new StringBuilder("09");
         System.out.println(d); // [29, 09, 1990]
     }
 }
+
+
+// clear() – очистка списка
+// toString() – «конвертация» списка в строку
+// Arrays.asList – преобразует массив в список
+// containsAll(col) – проверяет включение всех элементов из col
+// removeAll(col) – удаляет элементы, имеющиеся в col
+// retainAll(col) – оставляет элементы, имеющиеся в col
+// toArray() – конвертация списка в массив Object’ов
+// toArray(type array) – конвертация списка в массив type
+// List.copyOf(col) – возвращает копию списка на основе имеющегося
+// List.of(item1, item2,...) – возвращает неизменяемый список
