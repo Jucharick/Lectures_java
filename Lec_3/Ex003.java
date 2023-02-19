4357503a3b95202541c6163cde9c4a08ae73000b
Julia Ch <juliecha@yandex.ru>
2023-02-19 11:34:08 +0300

    UPD Ex003 - collection methods

--- Lec_3/Ex003.java
+++ Lec_3/Ex003.java
@@ -1,3 +1,20 @@
+// Ошибки на этапе компиляции лучше ошибок времени
+// выполнения
+// Повторное использование кода 
+
+
+// add(args) – добавляет элемент в список ( в т.ч. на нужную позицию)
+// get(pos) – возвращает элемент из списка по указанной позиции
+// indexOf(item) – первое вхождение или -1
+// lastIndexOf(item) – последнее вхождение или -1
+// remove(pos) – удаление элемента на указанной позиции и его возвращение
+// set(int pos, T item) – gjvtoftn значение item элементу, который находится
+// на позиции pos
+// void sort(Comparator) – сортирует набор данных по правилу
+// subList(int start, int end) – получение набора данных от позиции start до end
+
+
+
 package Lec_3;
 
 import java.util.ArrayList;
@@ -10,6 +27,7 @@ public class Ex003 {
         ArrayList<Integer> list4 = new ArrayList<>(list3);
         System.out.println(list3);
         list3.add(123);
+        System.out.println(list3);
         System.out.println(list4);
 
     }
