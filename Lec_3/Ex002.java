4d9fad500c035f124def57c4eb50df9550829e81
Julia Ch <juliecha@yandex.ru>
2023-02-19 11:30:54 +0300

    UPD Ex001

--- Lec_3/Ex002.java
+++ Lec_3/Ex002.java
@@ -1,10 +1,30 @@
+// List – пронумерованный набор элементов.
+// Пользователь этого интерфейса имеет точный контроль над тем,
+// где в списке вставляется каждый элемент.
+// Пользователь может обращаться к элементам по их целочисленному
+// индексу (позиции в списке) и искать элементы в списке.
+// ArrayList, LinkedList (Vector, Stack – устаревшие)
+
+
+
 package Lec_3;
 import java.util.ArrayList;
 public class Ex002 {
 
    public static void main(String[] args) {
-       ArrayList<Integer> list = new ArrayList<Integer>();
-       //ArrayList list = new ArrayList();
+        // Иерархия коллекций. ArrayList
+        // «Удобный массив»
+        // Разные способы создания
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>(10); // изначально в нашей коллекции будет хранилище по 10 элементов (изначально выделено паматя на 10 элементов, потом моно будет добавить)
+        ArrayList<Integer> list4 = new ArrayList<>(list3);
+    
+
+
+
+       ArrayList<Integer> list = new ArrayList<Integer>(); // <> - обобщение - использовать с обобщением, а не с сырыми типами
+       //ArrayList list = new ArrayList(); // неявное преобразование к object, сырой тип
        list.add(2809);
 
 
