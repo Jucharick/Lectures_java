a7f598956fe2a7ef29d7e491372ad31529036036
Julia Ch <juliecha@yandex.ru>
2023-03-01 08:52:09 +0300

    UPD Ex002_MathSet

--- Lec_6/part1/Ex002_MathSet.java
+++ Lec_6/part1/Ex002_MathSet.java
@@ -6,9 +6,15 @@ public class Ex002_MathSet {
         var a = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7));
         var b = new HashSet<>(Arrays.asList(2,3,5,7,11,13,17));
 
-        var u = new HashSet<Integer>(a); u.addAll(b); // addAll(Coll) – объединение множеств.
-        var r = new HashSet<Integer>(a); r.retainAll(b); // retainAll(Coll) – пересечение множеств.
-        var s = new HashSet<Integer>(a); s.removeAll(b); // removeAll(Coll) – разность множеств.
+        // работает не как в маниматеке => делается копия первого множества, а затем преобразовывется
+        var u = new HashSet<Integer>(a); 
+        u.addAll(b); // addAll(Coll) – объединение множеств.
+
+        var r = new HashSet<Integer>(a); 
+        r.retainAll(b); // retainAll(Coll) – пересечение множеств.
+
+        var s = new HashSet<Integer>(a); 
+        s.removeAll(b); // removeAll(Coll) – разность множеств.
 
         System.out.println(a); // [1, 2, 3, 4, 5, 6]
         System.out.println(b); // [17, 2, 3, 5, 7, 11]
